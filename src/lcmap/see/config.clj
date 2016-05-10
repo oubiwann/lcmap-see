95f6347958a33fc3d63584fe822f14bed7b1148b
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-05-10 05:27:27 -0500

    Use single cfg schema per project

--- src/lcmap/see/config.clj
+++ src/lcmap/see/config.clj
@@ -4,19 +4,16 @@
 
 (def opt-spec [])
 
-(def db-cfg-schema
-  {:db-hosts [schema/Str]
-   :db-user schema/Str
-   :db-pass schema/Str
-   :job-namespace schema/Str
-   :job-table schema/Str})
-
-(def msg-cfg-schema
-  {:msg-host schema/Str})
+(def see-schema
+  {:lcmap.see {:db-hosts [schema/Str]
+               :db-user schema/Str
+               :db-pass schema/Str
+               :job-namespace schema/Str
+               :job-table schema/Str}})
 
 (def cfg-schema
-  {:lcmap.see (merge db-cfg-schema msg-cfg-schema)
-   schema/Keyword schema/Any})
+  (merge see-schema
+         {schema/Keyword schema/Any}))
 
 (def defaults
   {:ini *lcmap-config-ini*
