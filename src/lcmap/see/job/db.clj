f17a4d713ede8e076695d615549dce721c9d9090
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-05-12 10:52:47 -0500

    Fix bad symbol name. Oops.

--- src/lcmap/see/job/db.clj
+++ src/lcmap/see/job/db.clj
@@ -33,7 +33,7 @@
     (query/limit 1)))
 
 (defn insert-default [conn job-id default-row]
-  (log/debugf "Saving %s to '%s' .." default-row jobs)
+  (log/debugf "Saving %s to '%s.%s' .." default-row job-keyspace job-table)
   (cql/use-keyspace conn job-keyspace)
   (cql/insert-async
     conn
