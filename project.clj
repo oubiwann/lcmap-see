b0cbcb0854d0df904454794e5c367d160b233eff
Duncan McGreggor <oubiwann@gmail.com>
2016-07-15 09:23:09 -0500

    Use 0.5.0 release of lcmap-config.

--- project.clj
+++ project.clj
@@ -29,9 +29,9 @@
                  ;; Distributed Computation
                  [clojusc/mesomatic "0.28.2-SNAPSHOT"]
                  ;; LCMAP Components
-                 [gov.usgs.eros/lcmap-config "0.5.0-SNAPSHOT"]
-                 [gov.usgs.eros/lcmap-logger "0.5.0-SNAPSHOT"]
+                 [gov.usgs.eros/lcmap-config "0.5.0"]
                  [gov.usgs.eros/lcmap-client-clj "0.5.0-SNAPSHOT"]
+                 [gov.usgs.eros/lcmap-logger "0.5.0-SNAPSHOT"]
                  ;; XXX note that we may still need to explicitly include the
                  ;; Apache Java HTTP client, since the version used by the LCMAP
                  ;; client is more recent than that used by Chas Emerick's
