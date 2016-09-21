5251339755250d3ae6625012a40c0353d931d72f
Duncan McGreggor <oubiwann@gmail.com>
2016-09-21 16:30:39 -0500

    Added resources functions.

---
+++ src/lcmap/see/backend/mesos/models/common/resources.clj
@@ -0,0 +1,78 @@
+(ns lcmap.see.backend.mesos.models.common.resources
+  ""
+  (:require [clojure.tools.logging :as log]
+            [clojure.string :as string]
+            [clojusc.twig :refer [pprint]]))
+
+(defn make-map
+  ""
+  [{cpus :cpus mem :mem}]
+  [{:name "cpus"
+    :scalar (if (nil? cpus) 1 cpus)
+    :type :value-scalar}
+   {:name "mem"
+    :scalar (if (nil? mem) 128 mem)
+    :type :value-scalar}])
+
+(defn cpus?
+  ""
+  [resource]
+  (if (= (:name resource) "cpus")
+    true
+    false))
+
+(defn mem?
+  ""
+  [resource]
+  (if (= (:name resource) "mem")
+    true
+    false))
+
+(defn update-cpus
+  ""
+  [data resource]
+  (if (cpus? resource)
+    (assoc data :cpus (-> resource
+                          :scalar
+                          (+ (or (:cpus data)))))
+    data))
+
+(defn update-mem
+  ""
+  [data resource]
+  (if (mem? resource)
+    (assoc data :mem (-> resource
+                         :scalar
+                         (+ (or (:mem data) 0))))
+    data))
+
+(defn trace-totals
+  ""
+  [data]
+  (log/tracef "Totalled %s cpus and %s mem from offer."
+              (:cpus data) (:mem data))
+  data)
+
+(defn update-cpus-mem
+  ""
+  [data resource]
+  (-> data
+      (update-cpus resource)
+      (update-mem resource)
+      (trace-totals)))
+
+(defn sum
+  ""
+  ([offer]
+    (sum offer {:cpus 0 :mem 0}))
+  ([offer init-data]
+    (reduce update-cpus-mem
+            init-data
+            (:resources offer))))
+
+(defn make
+  ""
+  [offer]
+  (-> offer
+      (sum)
+      (make-map)))
