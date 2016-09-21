b4dcb0ff6117f2a9446f54c02371d0cf402d28d7
Duncan McGreggor <oubiwann@gmail.com>
2016-09-21 12:33:28 -0500

    Added wrapper for handle-msg multi-method.
    This was adapted from @pyr's comment in the following Mesomatic ticket:
     * https://github.com/pyr/mesomatic/issues/15

--- src/lcmap/see/backend/mesos.clj
+++ src/lcmap/see/backend/mesos.clj
@@ -222,3 +222,21 @@
   (-> state
       (check-task-finished payload)
       (check-task-abort payload)))
+
+(defn wrap-handle-msg
+  "Wrap the handle-msg multi-method so that exceptions can be properly caught
+  and the scheduler can be given the change to perform an abort procedure.
+
+  Anticipated use of the function is:
+
+    (async.core/reduce (partial wrap-handle-msg handle-msg) { ... } ch)
+
+  For use with the Mesomatic async scheduler API."
+  [handler state payload]
+  (try
+    (handler state payload)
+    (catch Exception e
+      (log/error "Got error:" (.getMessage e))
+      (scheduler/abort! (:driver state))
+      (reduced
+        (assoc state :error e)))))
