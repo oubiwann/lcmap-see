10820d351603ff1ebf355402c622c2750a16a73f
Duncan McGreggor <oubiwann@gmail.com>
2016-09-21 20:38:50 -0500

    Experimental framework run.
    Before integration into LCMAP job tracker.

--- src/lcmap/see/backend/mesos/models/sample.clj
+++ src/lcmap/see/backend/mesos/models/sample.clj
@@ -3,20 +3,16 @@
   system local to the LCMAP REST server, capturing standard out, with a
   synthetic (and variable) delay introduced to show asynchronous results."
   (:require [clojure.tools.logging :as log]
-            [clj-commons-exec :as exec]
+            [lcmap.see.backend.mesos.models.sample.framework :as framework]
             [lcmap.see.job.tracker :as jt]))
-
-(defn long-running-func [[job-id sleep-time year]]
-  (log/debugf "\n\nRunning job %s (waiting for %s seconds) ...\n"
-                     job-id
-                     sleep-time)
-  @(exec/sh ["sleep" (str sleep-time)])
-  (:out @(exec/sh ["cal" year])))
 
 (defn run-model [component job-id default-row result-table seconds year]
   ;; Define some vars for pedagogical clarity
-  (let [func #'long-running-func
-        args [job-id seconds year]]
+  (let [args [component job-id seconds year]]
+    ;; run mesos framework
+    (apply framework/run args)
+    ;; get results
+    ;; save results to db
     (jt/track-job component
                   job-id
                   default-row
