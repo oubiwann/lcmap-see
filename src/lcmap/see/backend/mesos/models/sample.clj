18296efbcd477a1d67ec82116e2b45f9f051717d
Duncan McGreggor <oubiwann@gmail.com>
2016-09-27 07:42:29 -0500

    Updated the Mesos framework to use the new Mesos tracker impl.

--- src/lcmap/see/backend/mesos/models/sample.clj
+++ src/lcmap/see/backend/mesos/models/sample.clj
@@ -1,20 +1,24 @@
 (ns lcmap.see.backend.mesos.models.sample
-  "This sample runner demonstrates kicking off a job that is executed on the
-  system local to the LCMAP REST server, capturing standard out, with a
-  synthetic (and variable) delay introduced to show asynchronous results."
+  "This sample runner demonstrates kicking off a job that is executed on
+  Mesos with a synthetic (and variable) delay introduced to show asynchronous
+  results."
   (:require [clojure.tools.logging :as log]
             [lcmap.see.backend.mesos.models.sample.framework :as framework]
-            [lcmap.see.job.tracker :as jt]))
+            [lcmap.see.job.tracker :as tracker]
+            [lcmap.see.job.tracker.mesos]))
 
 (defn run-model [component job-id default-row result-table seconds year]
   ;; Define some vars for pedagogical clarity
-  (let [args [component job-id seconds year]]
+  (let [tracker-impl (get-in component [:see :job :tracker])
+        func #'framework/run
+        args [job-id seconds year]]
     ;; run mesos framework
     (apply framework/run args)
     ;; get results
     ;; save results to db
-    (jt/track-job component
-                  job-id
-                  default-row
-                  result-table
-                  [func args])))
+    (tracker/track-job
+      tracker-impl
+      job-id
+      default-row
+      result-table
+      [func args])))
