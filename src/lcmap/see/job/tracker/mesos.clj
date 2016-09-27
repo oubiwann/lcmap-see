9f20d3f8e5698297e3232acb42c529b4abf5e212
Duncan McGreggor <oubiwann@gmail.com>
2016-09-27 07:33:08 -0500

    Added initial tracker for Mesos.

---
+++ src/lcmap/see/job/tracker/mesos.clj
@@ -0,0 +1,67 @@
+(ns lcmap.see.job.tracker.mesos
+  ""
+  (:require [clojure.tools.logging :as log]
+            [co.paralleluniverse.pulsar.core :refer [defsfn]]
+            [co.paralleluniverse.pulsar.actors :as actors]
+            [lcmap.client.status-codes :as status]
+            [lcmap.see.job.db :as db]
+            [lcmap.see.job.tracker :as tracker])
+  (:refer-clojure :exclude [promise await bean])
+  (:import [co.paralleluniverse.common.util Debug]
+           [co.paralleluniverse.actors LocalActor]
+           [co.paralleluniverse.strands Strand]))
+
+;;; Implementation overrides for native tracker ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsfn run-job
+  [this {job-id :job-id [job-func job-args] :result :as args}]
+  (log/debugf "Running the job with function %s and args %s ..."
+              job-func
+              job-args)
+  ;; XXX What's the best way to capture the results from a Mesos point of view?
+  ;;     We'll probably need to send the result when the appropriate async
+  ;;     handler fires ... so the handler might need a reference to the protocol
+  ;;     implementation ... in which case *it* can notify the :event-thread
+  ;;     of results available to save ...
+  (job-func job-args)
+  (log/debugf "Kicked off Mesos framework.")
+  @(db/update-status (:db-conn this) job-id status/pending-link)
+
+  ;; XXX Maybe split this into two functions?
+  ;;     - #'start-run-job (and state transition :job-start-run)
+  ;;     - #'finish-run-job (and state transition :job-finish-run)
+  ;;     This would allow us to more easily handle process other async execution
+  ;;     frameworks for results. It will mean that the transitions (and related
+  ;;     functions) will need to be updated (replacing :run-job with the two new
+  ;;     transitions ...)
+  (log/debug "Finished job.")
+  (actors/notify! (:event-thread this)
+                  (into args {:type :job-save-data
+                              :result job-data}))))
+
+(defsfn dispatch-handler
+  [this {type :type :as args}]
+  (case type
+    :job-track-init (tracker/init-job-track this args)
+    :job-result-exists (tracker/return-existing-result this args)
+    :job-run (run-job this args)
+    :job-save-data (tracker/save-job-data this args)
+    :job-track-finish (tracker/finish-job-track this args)
+    :done (tracker/done this args)))
+
+;;; Native protocol setup ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def jobable-behaviour
+  (merge
+    tracker/jobable-default-behaviour
+    {:run-job #'run-job}))
+
+(defrecord MesosTracker [name cfg db-conn event-thread])
+
+(extend MesosTracker tracker/ITrackable tracker/trackable-default-behaviour)
+(extend MesosTracker tracker/IJobable jobable-behaviour)
+
+(defn new-tracker
+  ""
+  [cfg db-conn event-thread]
+  (->MesosTracker :native cfg db-conn event-thread))
