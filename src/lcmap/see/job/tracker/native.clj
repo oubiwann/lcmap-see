fdb77ec3b0c6d556f5f8a80b70526ee8fe064a4e
Duncan McGreggor <oubiwann@gmail.com>
2016-10-20 13:24:36 -0500

    Fixed the issue with Mesos models not creating proper hashes.
    Now, when a model has already run, the job id created will match that of a
    previous run with the same function and same args. The issue was due to the
    fact that the default hasher didn't take into account that Mesos model run
    functions have additional args prepended to them.

--- src/lcmap/see/job/tracker/native.clj
+++ src/lcmap/see/job/tracker/native.clj
@@ -13,7 +13,20 @@
            [co.paralleluniverse.actors LocalActor]
            [co.paralleluniverse.strands Strand]))
 
-;;; Implementation overrides for native tracker ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Implementation overrides for native job ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsfn gen-hash
+  "Generate a unique hash for science model function and specific parameters
+  provided for the science model. This allows the SEE to provide results that
+  have already been generated once without having to recalculate them."
+  [this func args]
+  (log/debugf "Preparing to hash [func args]: [%s %s]" func args)
+  (-> func
+      (str)
+      (vector)
+      (into args)
+      (str)
+      (digest/md5)))
 
 (defsfn start-job-run
   [this {job-id :job-id [job-func job-args] :result :as args}]
@@ -46,17 +59,43 @@
     :job-track-finish (tracker/finish-job-track this args)
     :job-done (tracker/done this args)))
 
+;;; Implementation overrides for native tracker ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defsfn track-job
+  [this model-func model-args]
+  (log/debug "Preparing to track job ...")
+  (let [db-conn (:db-conn this)
+        job-id (gen-hash this model-func model-args)
+        default-row (db/make-default-row (:cfg this) job-id (:name this))]
+    (log/debug "Generated model run hash (job-id):" job-id)
+    (log/trace "Generated default-row:" (pprint default-row))
+    (log/trace "Using event server" (:event-thread this) "with db connection"
+               db-conn)
+    (tracker/send-msg
+      this {
+        :type :job-track-init
+        :job-id job-id
+        :default-row default-row
+        :result [model-func model-args]})
+    job-id))
+
 ;;; Native protocol setup ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def trackable-behaviour
+  (merge
+    tracker/trackable-default-behaviour
+    {:track-job #'track-job}))
 
 (def jobable-behaviour
   (merge
     tracker/jobable-default-behaviour
-    {:start-job-run #'start-job-run
+    {:gen-hash #'gen-hash
+     :start-job-run #'start-job-run
      :finish-job-run #'finish-job-run}))
 
 (defrecord NativeTracker [name cfg db-conn event-thread])
 
-(extend NativeTracker tracker/ITrackable tracker/trackable-default-behaviour)
+(extend NativeTracker tracker/ITrackable trackable-behaviour)
 (extend NativeTracker tracker/IJobable jobable-behaviour)
 
 (defn new-tracker
